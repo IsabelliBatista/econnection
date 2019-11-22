7f7e517adcaee59b96850f83868ed78cdd2fa44d
ingridfs <ingrid.fernandes.santos@hotmail.com>
2019-11-21 22:12:29 -0300

    adding files

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="bg-dark container-fluid">
+  <section class="row">
+    <article class="col-sm">
+      Links
+    </article>
+    <article class="col-sm">
+      Links
+    </article>
+    <article class="col-sm">
+      Redes Sociais
+    </article>
+  </section>
+</footer>
