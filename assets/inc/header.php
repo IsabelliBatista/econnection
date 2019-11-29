19ea9ec844efe50274d51699b498390b28dd1f1e
ingridfs <ingrid.fernandes.santos@hotmail.com>
2019-11-28 23:37:15 -0300

    adding banner, about and product cards

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,57 +1,40 @@
 <header>
-  <div class="container justify-content-center">
-      <a class="navbar-brand" href="#">
-          <img src="assets/img/logo.png" alt="Logo do meu projeto" height="60" width="160">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-        </form>
+  <div class="container d-flex justify-content-between mt-2 mb-2">
+    <a class="navbar-brand">LOGO</a>
+    <div class="d-flex align-items-center">
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
+      <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+    </form>
+      <div class="align-items-between">
+          <a href="login.php" class="text-reset"><i class="fas fa-user"></i></a>
+          <a href="carrinho.php" class="text-reset"><i class="fas fa-shopping-cart"></i></a>    
+      </div>
+    </div>
   </div>
-  <!-- Menu -->
-  <ul class="nav nav-tabs justify-content-right">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Produtos</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Ação</a>
-          <a class="dropdown-item" href="#">Outra ação</a>
-          <a class="dropdown-item" href="#">Algo mais aqui</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Link isolado</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categorias</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Ação</a>
-            <a class="dropdown-item" href="#">Outra ação</a>
-            <a class="dropdown-item" href="#">Algo mais aqui</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Link isolado</a>
-          </div>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav justify-content-around">
+        <li class="nav-item text-white active">
+          <a class="nav-link text-uppercase" href="#">Casa</a>
         </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Vendedores</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Ação</a>
-              <a class="dropdown-item" href="#">Outra ação</a>
-              <a class="dropdown-item" href="#">Algo mais aqui</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Link isolado</a>
-            </div>
+        <li class="nav-item text-white">
+          <a class="nav-link text-uppercase" href="#">Beleza</a>
         </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sobre Nós</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Ação</a>
-              <a class="dropdown-item" href="#">Outra ação</a>
-              <a class="dropdown-item" href="#">Algo mais aqui</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Link isolado</a>
-            </div>
+        <li class="nav-item text-white">
+          <a class="nav-link text-uppercase" href="#">100Plástico</a>
         </li>
-    </ul>
+        <li class="nav-item text-white">
+          <a class="nav-link text-uppercase" href="#">Acessórios</a>
+        </li>
+        <li class="nav-item text-white">
+          <a class="nav-link text-uppercase" href="#">Marcas</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </header>
--- index.php
+++ index.php
@@ -41,27 +41,91 @@
     <title>eConnection | Produtos Sustentáveis</title>
     
     <!-- Estilo Bootstrap -->
-    <link async defer rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
     <!-- Estilo FontAweomse -->
-    <link async defer rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Nosso estilo -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-  <header>
     <?php require_once("assets/inc/header.php"); ?>
-  </header>
-
   <main>
-    <header></header>
+    <!-- BANNER COM CARROSSEL -->
+    <section class="banner">
+      <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="assets/img/canudo-inox.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="assets/img/copo-retratil.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="assets/img/eco-bag.png" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+      </div>
+    </section>
+
+    <!-- CARD COM TEXTO QUEM SOMOS -->
+    <section class="container p-10">
+      <div class="row no-gutters bg-light position-relative">
+        <div class="col-md-6 position-static p-4 pl-md-0">
+          <h5 class="mt-0">Columns with stretched link</h5>
+          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+        </div>
+        <div class="col-md-6 mb-md-0 p-md-4">
+          <img src="assets/img/quem-somos.png" class="w-100" alt="Quem somos">
+        </div>
+      </div>
+    </section>
+    
+    <!-- CARD PRODUTOS EM DESTAQUE -->
+    <section class="container">
+        <div class="card-deck">
+            <div class="card">
+              <img src="assets/img/destaq-canudo.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src="assets/img/destaq-ecobag.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src="assets/img/destq-copo.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+    </section>
+
   </main>
   <footer>
     <?php require_once("assets/inc/footer.php"); ?>
   </footer>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
