9d59994828e61421a21809ca5ca1132f0ffe011d
IsabelliBatista <48300634+IsabelliBatista@users.noreply.github.com>
2019-12-01 12:41:39 -0300

    Add files via upload

---
+++ Projeto-Integrador-DH-master/assets/inc/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer container-fluid text-white mt-5">
+    <div class="container p-4">
+      <h4 class="text-center mt-3 mb-5">RECEBA NOSSAS OFERTAS E NOVIDADES POR E-MAIL.</h4>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Digite seu e-mail aqui." aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">ASSINE</button>
+        </div>
+      </div>
+    </div>
+  <div class="container pb-3">
+    <div class="row">
+      <div class="col-sm">
+        <h6 class="text-uppercase mb-3">Categorias</h6>
+        <p><a href="#" class="text-white-50">Casa</a></p>
+        <p><a href="#" class="text-white-50">Cuidados</a>
+        <p><a href="#" class="text-white-50">100Plástico</a>
+        <p><a href="#" class="text-white-50">Acessórios</a>
+      </div>
+      <div class="col-sm">
+        <h6 class="text-uppercase mb-3">Ajuda</h6>
+        <p><a href="#" class="text-white-50">Dúvidas Frequentes</a></p>
+        <p><a href="#" class="text-white-50">Atendimento</a></p>
+        <p><a href="#" class="text-white-50">Mapa do Site</a></p>
+        <p><a href="#" class="text-white-50">Glossário da Sustentabilidade</a></p>
+      </div>
+      <div class="col-sm">
+        <h6 class="text-uppercase mb-3">eConnection</h6>
+        <p><a href="#" class="text-white-50">Sobre nós</a></p>
+        <p><a href="#" class="text-white-50">Institucional</a></p>
+        <p><a href="#" class="text-white-50">Seja nosso parceiro</a></p>
+        <p><a href="#" class="text-white-50">Políticas de troca e devoluções</a></p>
+        </div>
+      <div class="col-sm">
+        <h6 class="text-uppercase mb-3">Social</h6>
+        <a href="#" class="p-2 text-reset"><i class="fab fa-facebook"></i></a>
+        <a href="#" class="p-2 text-reset"><i class="fab fa-twitter"></i></a>    
+        <a href="#" class="p-2 text-reset"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ Projeto-Integrador-DH-master/assets/inc/header.php
@@ -0,0 +1,102 @@
+<header>
+  <div class="container-md d-flex justify-content-between mt-1 mb-1">
+    <a class="navbar-brand" href="#">
+      <img src="assets/img/logo2.1.png" alt="Logo eConnection">
+    </a>
+    <div class="d-flex align-items-center ">
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
+        <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+      </form>
+    <span>
+        <a href="login.php" class="p-2 text-reset"><i class="fas fa-user"></i></a>
+        <a href="carrinho.php" class="p-2 text-reset"><i class="fas fa-shopping-cart"></i></a>    
+    </span>
+    </div>
+  </div>
+  <div>
+  <nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav justify-content-around">
+        <li class="nav-item  active">
+            <div class="dropdown">
+                <button class="btn text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a href="../index.php">Início</a>
+                </button>
+            </div>
+        </li>
+        <li class="nav-item  active">
+            <div class="dropdown">
+                <button class="btn text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a href="../quemsomos.php">Quem Somos</a>
+                </button>
+            </div>
+        </li>
+        <li class="nav-item  active">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Casa
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Composteira Residencial</a>
+                  <a class="dropdown-item" href="#">Limpeza Ecológica</a>
+                  <a class="dropdown-item" href="#">Rotina Sustentável</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item ">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Cuidados 
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Cabelos</a>
+                  <a class="dropdown-item" href="#">Barbear Clássico</a>
+                  <a class="dropdown-item" href="#">Higiene Bucal</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item ">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  100 Plástico
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Canudos e Copos Reutilizáveis</a>
+                  <a class="dropdown-item" href="#">Ecobags, Kit Hortifruti e Compras a granel</a>
+                  <a class="dropdown-item" href="#">Kits Lixo Zero</a>
+                </div>
+            </div>        
+        </li>
+        <li class="nav-item ">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Acessórios
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Acessórios para Yoga e Meditação</a>
+                  <a class="dropdown-item" href="#">Brinquedos Educativos</a>
+                  <a class="dropdown-item" href="#">Decoração</a>
+                </div>
+            </div> 
+        </li>
+        <li class="nav-item ">
+            <div class="dropdown">
+                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Marcas
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">BeeGreen</a>
+                  <a class="dropdown-item" href="#">Ideia Crua</a>
+                  <a class="dropdown-item" href="#">Korui</a>
+                </div>
+            </div> 
+        </li>
+      </ul>
+    </div>
+  </nav>
+  </div>
+</header>
