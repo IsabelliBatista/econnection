e737d465fbfb9ff79e82a35ba0e8dc428870a67b
Isabelli Batista <isabellibatista7@gmail.com>
2020-01-09 14:15:31 -0300

    arrumando index

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
   </div> -->
 
   
-  <!-- CARD PRODUTOS EM DESTAQUE --> 
+  <!-- CARD PRODUTOS --> 
   <div class="card-produtos">
     <section class="container mt-5 ">
         <h2 class="text-bold mt-5 mb-5"> 
@@ -151,7 +151,47 @@
           </div>
     </section>
 
+    <!--BANNER-->
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
 
+<!--PARCEIROS-->
     <section class="container mt-5 ">
         <h2 class="text-bold mt-5 mb-5"> 
           Alguns Parceiros
@@ -186,101 +226,7 @@
           </div>
       </section>
     </div>
-  <!-- IMAGENS COM OS PRODUTOS TOPS -->
-  <!-- <div class="container">
-      <div class="row">
-        <div class="medium-4 columns">
-            <h4>Top Products</h4>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="assets/img/tops_posts.jpg">
-              </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-        </div>
-        <div class="medium-4 columns">
-            <h4>Top Products</h4>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="assets/img/tops_posts.jpg">
-              </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ulla</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-        </div>
-        <div class="medium-4 columns">
-            <h4>Top Products</h4>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="assets/img/tops_posts.jpg">
-              </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ull</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-          <div class="media-object">
-            <div class="media-object-section">
-              <img class="thumbnail" src="assets/img/tops_posts.jpg">
-            </div>
-            <div class="media-object-section">
-              <h5>Nunc Eu Ulla</h5>
-              <p>Quisque eget odio ac lectus vestibulum</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
+
   </main>
     <?php require_once("assets/inc/footer.php"); ?>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
