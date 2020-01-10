f1465b71e44d90dcd044993d0015cdb1b966f013
Isabelli Batista <isabellibatista7@gmail.com>
2020-01-10 16:41:01 -0300

    arrumando alguns itens do header e outros

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,109 +1,103 @@
 <header>
+
   <div class="container-md d-flex justify-content-between mt-1 mb-1 div-header-itens">
-    <a class="navbar-brand" href="#">
-      <img src="assets/img/logo.png" alt="Logo eConnection">
-    </a>
-    <div class="d-flex align-items-center ">
+    <div>
+      <a class="logo-nav" href="index.php">
+        <img src="assets/img/logo.png" alt="Logo eConnection">
+      </a>
+    </div>
+    <div class="icons-nav">
       <form class="form-inline">
         <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
         <button class="btn" type="submit"><i class="fas fa-search"></i></button>
       </form>
-    <span>
-        <a href="login.php" class="p-2 text-reset"><i class="fas fa-user"></i></a>
-        <a href="carrinho.php" class="p-2 text-reset"><i class="fas fa-shopping-cart"></i></a>    
-    </span>
+      <span>
+          <a href="login.php" class="p-2 text-reset"><i class="fas fa-user"></i></a>
+          <a href="carrinho.php" class="p-2 text-reset"><i class="fas fa-shopping-cart"></i></a>    
+      </span>
     </div>
   </div>
+  
+
   <div>
-  <nav class="navbar navbar-expand-lg">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-      <ul class="navbar-nav justify-content-around">
-        <li class="nav-item  active">
-            <div class="dropdown">
-                <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
-                  <a class="btn text-white text-uppercase" href="./index.php">Início</a>
-                <!-- </button> -->
-            </div>
-        </li>
-        <li class="nav-item  active">
-            <div class="dropdown">
-                <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
-                  <a href="./quemsomos.php" class="btn text-white text-uppercase">Quem Somos</a>
-                <!-- </button> -->
-            </div>
-        </li>
-        <li class="nav-item  active">
-            <div class="dropdown">
-                <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
-                  <a href="./parceiros.php" class="btn text-white text-uppercase">Parceiros</a>
-                <!-- </button> -->
-            </div>
-        </li>
-        <li class="nav-item  active">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Casa
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">Composteira Residencial</a>
-                  <a class="dropdown-item" href="#">Limpeza Ecológica</a>
-                  <a class="dropdown-item" href="#">Rotina Sustentável</a>
-                </div>
-            </div>
-        </li>
-        <li class="nav-item ">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Cuidados 
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">Cabelos</a>
-                  <a class="dropdown-item" href="#">Barbear Clássico</a>
-                  <a class="dropdown-item" href="#">Higiene Bucal</a>
-                </div>
-            </div>
-        </li>
-        <li class="nav-item ">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  100 Plástico
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">Canudos e Copos Reutilizáveis</a>
-                  <a class="dropdown-item" href="#">Ecobags, Kit Hortifruti e Compras a granel</a>
-                  <a class="dropdown-item" href="#">Kits Lixo Zero</a>
-                </div>
-            </div>        
-        </li>
-        <li class="nav-item ">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Acessórios
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">Acessórios para Yoga e Meditação</a>
-                  <a class="dropdown-item" href="#">Brinquedos Educativos</a>
-                  <a class="dropdown-item" href="#">Decoração</a>
-                </div>
-            </div> 
-        </li>
-        <li class="nav-item ">
-            <div class="dropdown">
-                <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Marcas
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">BeeGreen</a>
-                  <a class="dropdown-item" href="#">Ideia Crua</a>
-                  <a class="dropdown-item" href="#">Korui</a>
-                </div>
-            </div> 
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <nav class="navbar navbar-expand-lg">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav justify-content-around">
+          <li class="nav-item  active">
+              <div class="dropdown">
+                  <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
+                    <a class="btn text-white text-uppercase" href="./index.php">Início</a>
+                  <!-- </button> -->
+              </div>
+          </li>
+          <li class="nav-item  active">
+              <div class="dropdown">
+                  <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
+                    <a href="./quemsomos.php" class="btn text-white text-uppercase">Quem Somos</a>
+                  <!-- </button> -->
+              </div>
+          </li>
+          <li class="nav-item  active">
+              <div class="dropdown">
+                  <!-- <button class="btn text-white text-uppercase" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
+                    <a href="./parceiros.php" class="btn text-white text-uppercase">Parceiros</a>
+                  <!-- </button> -->
+              </div>
+          </li>
+          <li class="nav-item  active">
+              <div class="dropdown">
+                  <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Casa
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Composteira Residencial</a>
+                    <a class="dropdown-item" href="#">Limpeza Ecológica</a>
+                    <a class="dropdown-item" href="#">Rotina Sustentável</a>
+                  </div>
+              </div>
+          </li>
+          <li class="nav-item ">
+              <div class="dropdown">
+                  <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Cuidados 
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Cabelos</a>
+                    <a class="dropdown-item" href="#">Barbear Clássico</a>
+                    <a class="dropdown-item" href="#">Higiene Bucal</a>
+                  </div>
+              </div>
+          </li>
+          <li class="nav-item ">
+              <div class="dropdown">
+                  <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    100 Plástico
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Canudos e Copos Reutilizáveis</a>
+                    <a class="dropdown-item" href="#">Ecobags, Kit Hortifruti e Compras a granel</a>
+                    <a class="dropdown-item" href="#">Kits Lixo Zero</a>
+                  </div>
+              </div>        
+          </li>
+          <li class="nav-item ">
+              <div class="dropdown">
+                  <button class="btn dropdown-toggle text-white text-uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Acessórios
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Acessórios para Yoga e Meditação</a>
+                    <a class="dropdown-item" href="#">Brinquedos Educativos</a>
+                    <a class="dropdown-item" href="#">Decoração</a>
+                  </div>
+              </div> 
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
+
 </header>
