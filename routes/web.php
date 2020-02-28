2c92f3c637eba273add5734447681e1b9bdff9f9
Isabelli Batista <isabellibatista7@gmail.com>
2020-02-28 14:13:21 -0300

    adicionando o node.js

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
