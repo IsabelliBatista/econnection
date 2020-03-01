be931414c239841019f8034b959886863381c451
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-03-01 13:46:57 -0300

    Configurando Projeto

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
