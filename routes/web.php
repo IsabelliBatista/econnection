c2067440d798be2c08ddafa55f04fd377159ac72
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-03-01 14:54:15 -0300

    Adicionado o head no layouts>app

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/index', function(){
+    return view('index');
+});
