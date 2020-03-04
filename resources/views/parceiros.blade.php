795514defa79a7c9dda4c8cfd7cab674cddc0cd7
isabelli batista <isabellibatista7@gmail.com>
2020-03-04 20:03:14 -0300

    adicionando pagina carrinho na view

---
+++ resources/views/parceiros.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.master')
+
+@section('title')
+    Carrinho - eConnection
+@endsection
+
+@section('content')
+<article class="bg-header-somos"><h1 class="text-center container">Parceiros</h1></article>
+
+<!-- CAMINHO DE PÁGINAS-->
+<div class="container">
+    <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Parceiros</li>
+    </ol>
+    </nav>
+</div>
+<section class="container mt-5 ">
+    <div>
+        <p class="text-center">Somos um Marketplace de empresas sustentáveis</p>
+    </div>
+<div class="row row-cols-1 row-cols-md-3">
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro1.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro2.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro3.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="container mt-5 mb-5">
+<div class="row row-cols-1 row-cols-md-3">
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro1.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro2.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card border-light h-100 text-center">
+            <img src="{{asset ('/img/Parceiro3.jpg')}}" class="card-img-top" alt="Canudo" width="300" height="200">
+            <div class="card-body">
+            <h5 class="card-title">Produto</h5>
+            <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+        </div>
+    </div>
+</section>
+    <div class="container banner-card ser-parceiro">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{asset ('/img/fundo3.jpg')}}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="mb-5">Seja Nosso Parceiro</h5>
+                        <p class="mb-5">Clique aqui para saber mais</p>
+                        <a href="contact.php" class="btn btn-card mb-4">Saber Mais</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
