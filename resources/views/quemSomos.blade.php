658ba1f0788622883813a3145f39a06c09728e57
isabelli batista <isabellibatista7@gmail.com>
2020-03-04 20:09:15 -0300

    adicionando pagina quemsomos na view

---
+++ resources/views/quemSomos.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.master')
+
+@section('title')
+    Quem Somos - eConnection
+@endsection
+
+@section('content')
+  	
+<header class="header-somos">
+    <article class="bg-header-somos"><h1 class="text-center container">Quem Somos</h1></article>
+
+<!-- CAMINHO DE PÁGINAS-->
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Quem Somos</li>
+        </ol>
+    </nav>
+</div>
+
+    <section class="container">
+        <h2 class="text-center">EConnection</h2>
+        <article class="container">
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.</p>
+        </article>
+    </section>
+</header>
+<main class="mt-5 main-somos">
+    <article class="">
+        <div class="container d-flex">
+            <h5 class="main-somos-h5">Objetivos</h5>
+            <div class="barra"></div>
+            <div class="barra2"></div>
+            <p class="align-self-center">Lorem Ipsum é simplesmente bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao</p>
+        </div>
+    </article>
+        <!-- CARD COM EXPLICAÇÃO BREVE -->
+        <section class="container row row-cols-1 row-cols-md-3 home-section-card text-center mt-5 mb-5">
+        <div class="col mb-4">
+            <div class="card border-light h-100">
+            <img src="{{asset ('/img/itens1.png')}}" class="m-auto" alt="e-connection" width="100" height="100">
+            <div class="card-body">
+                <h5 class="card-title">Marketplace</h5>
+                <p class="card-text">Somos um Marketplace de empresas sustentáveis</p>
+            </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card border-light h-100">
+            <img src="{{asset ('/img/itens2.png')}}" class="m-auto" alt="e-connection"  width="100" height="100">
+            <div class="card-body">
+                <h5 class="card-title">Produtos Sustentáveis</h5>
+                <p class="card-text">Fortaler as marcas e Divulgar produtos ecológicos</p>
+            </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card border-light h-100">
+            <img src="{{asset ('/img/itens3.png')}}" class="m-auto" alt="e-connection"  width="100" height="100">
+            <div class="card-body">
+                <h5 class="card-title">Loja</h5>
+                <p class="card-text">Encontre aqui uma loja perto de você</p>
+            </div>
+            </div>
+        </div>
+        </section>
+</main>
+<div class="divisor"></div>
+@endsection
