51c9b0be388a39f4e6214760f4b19a0207acbc69
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-12 21:29:53 -0300

    adicionando model endereço entrega

--- app/EnderecoEntrega.php
+++ app/EnderecoEntrega.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnderecoEntrega extends Model
 {
-    protected $table = 'filmes';
+    protected $table = 'endereco_entrega';
     protected $primaryKey = 'id';
     protected $fillable = [
         'rua', 'numero', 'bairro', 'cidade', 'cep', 'complemento'
