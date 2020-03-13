cf5259415280b7cedd0e6e5e48f50bb95d5efb25
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-12 21:34:17 -0300

    adicionando modal de categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $table = 'categoria';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nome'
+    ];
+
 }
