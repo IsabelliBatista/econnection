719c537b883e6b532d0c21632238cdef7f1a7c22
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-12 21:32:54 -0300

    adicionando modal do Vendedor

--- app/Vendedor.php
+++ app/Vendedor.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vendedor extends Model
 {
-    //
+    protected $table = 'vendedor';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'razao_social', 'nome_fantasia', 'cnpj', 'telefone', 'email', 'senha', 'endereco_id'
+    ];
+
+    public function endereco(){
+        return $this->hasOne('App\Endereco', 'id', 'endereco_id');
+    }
+
 }
