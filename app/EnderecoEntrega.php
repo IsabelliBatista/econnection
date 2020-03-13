7b2f47e54492f5a8284af9a9f77a1b375c489e60
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-12 21:28:31 -0300

    adicionando modal de Pedidos e EnderecoEntrega

--- app/EnderecoEntrega.php
+++ app/EnderecoEntrega.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnderecoEntrega extends Model
 {
-    //
+    protected $table = 'filmes';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'rua', 'numero', 'bairro', 'cidade', 'cep', 'complemento'
+    ];
 }
--- app/Pedidos.php
+++ app/Pedidos.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedidos extends Model
 {
-    //
+    protected $table = 'pedidos';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'status_pedido'
+    ];
+
+    public function clientes(){
+        return $this->hasOne('App\Cliente', 'id', 'clientes_id');
+    }
+    public function produtos(){
+        return $this->hasOne('App\Produto', 'id', 'produtos_id');
+    }
+    public function vendedor(){
+        return $this->hasOne('App\Vendedor', 'id', 'vendedor_id');
+    }
 }
