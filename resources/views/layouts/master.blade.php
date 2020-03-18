1b343fa1b19e1d0188db7f4d3edb3cac99961122
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-18 18:41:46 -0300

    adicionando links no dropbox

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,7 +90,10 @@
 
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="/carrinho">Meus Pedidos</i></a>    
-                <a class="dropdown-item" href="/listar/usuario">Meu Perfil</i></a>    
+                <a class="dropdown-item" href="/listar/usuario">Meu Perfil</i></a>
+                <a class="dropdown-item" href="/cadastrarProdutos">Cadastrar Produtos</i></a>
+                <a class="dropdown-item" href="/adicionandoC">Cadastrar Categorias</i></a>
+                <a class="dropdown-item" href="/adicionandoM">Cadastrar Marcas</i></a>    
                   <a class="dropdown-item" href="{{ route('logout') }}"
                       onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
