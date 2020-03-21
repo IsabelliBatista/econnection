81be394d742276784adf0568aaa3624579433dac
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-03-21 19:05:38 -0300

    Atualizando blade cadastrar usuarios

--- resources/views/usuario/cadastrarUsuario.blade.php
+++ resources/views/usuario/cadastrarUsuario.blade.php
@@ -15,91 +15,62 @@
           <form method="POST" action="/cadastrarUsuario">
             @csrf
             {{ method_field('POST') }}
-            <div class="form-group row">
-              <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome Completo:') }}</label>
-
-              <div class="col-md-6">
+            <div class="form-row mb-2">
+              <div class="col">
+                <label for="name">{{ __('Nome Completo:') }}</label>
                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                @error('name')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                @enderror
+                  @error('name')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
               </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail:') }}</label>
-
-              <div class="col-md-6">
+              <div class="col">
+                <label for="email">{{ __('Email:') }}</label>
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                @error('email')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                @enderror
-              </div>
+                  @error('email')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror             
+               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha:') }}</label>
-
-              <div class="col-md-6">
+            <div class="form-row mb-2">
+              <div class="col">
+                <label for="password">{{ __('Senha:') }}</label>
                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                  @error('password')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+              </div>
+              <div class="col">
+                <label for="password-confirm">{{ __('Confirme a senha:') }}</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">            
+               </div>
+            </div>
 
-                @error('password')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                @enderror
+            <div class="form-row mb-2">
+              <div class="form-group col-md-6">
+                <label for="cpf">{{ __('CPF:') }}</label>
+                <input id="cpf" type="text" class="form-control" name="cpf" value="{{ old('cpf') }}" required autocomplete="cpf">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="telefone">{{ __('Telefone:') }}</label>
+                <input id="telefone" type="text" class="form-control"  name="telefone" value="{{ old('telefone') }}" required autocomplete="telefone">
               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Senha:') }}</label>
-
-              <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            <div class="form-row mb-3">
+              <div class="col">
+                <label for="dataNasc">{{ __('Data de Nascimento:') }}</label>
+                <input id="dataNasc" type="date" class="form-control"  name="dataNasc" value="{{ old('dataNasc') }}" autocomplete="dataNasc">
               </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="cpf" class="col-md-4 col-form-label text-md-right">{{ __('CPF:') }}</label>
-
-              <div class="col-md-6">
-              <input id="cpf" type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" value="{{ old('cpf') }}" required autocomplete="cpf" autofocus>
-
-              @error('cpf')
-                <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-                </span>
-              @enderror
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="telefone" class="col-md-4 col-form-label text-md-right">{{ __('Telefone:') }}</label>
-
-              <div class="col-md-6">
-                <input id="telefone" type="text" class="form-control"  name="telefone" value="{{ old('telefone') }}" required autocomplete="telefone" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="dataNasc" class="col-md-4 col-form-label text-md-right">{{ __('Data de Nascimento:') }}</label>
-
-              <div class="col-md-6">
-                <input id="dataNasc" type="date" class="form-control"  name="dataNasc" value="{{ old('dataNasc') }}" autocomplete="dataNasc" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="sexo" class="col-md-4 col-form-label text-md-right">{{ __('Gênero:') }}</label>
-
-              <div class="col-md-6">
-                <select id="sexo" class="form-control" name="sexo" value="{{ old('sexo') }}" required autocomplete="sexo" autofocus>
+              <div class="col">
+                <label for="sexo">{{ __('Gênero:') }}</label>
+                <select id="sexo" class="form-control" name="sexo" value="{{ old('sexo') }}" required autocomplete="sexo">
                   <option selected>Selecione seu Gênero</option>
                   <option>Feminino</option>
                   <option>Masculino</option>
@@ -108,68 +79,45 @@
               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="cep" class="col-md-4 col-form-label text-md-right">{{ __('CEP:') }}</label>
-
-              <div class="col-md-6">
-                <input id="cep" type="text" class="form-control"  name="cep" value="{{ old('cep') }}" required autocomplete="cep" autofocus>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="rua">{{ __('Endereço:') }}</label>
+                <input id="rua" type="text" class="form-control"  name="rua" value="{{ old('rua') }}" required autocomplete="rua">
+              </div>
+              <div class="form-group col-md-2">
+                <label for="numero">{{ __('Número:') }}</label>
+                <input id="numero" type="text" class="form-control"  name="numero" value="{{ old('numero') }}" required autocomplete="numero">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="bairro">{{ __('Bairro:') }}</label>
+                <input id="bairro" type="text" class="form-control"  name="bairro" value="{{ old('bairro') }}" required autocomplete="bairro">
               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="rua" class="col-md-4 col-form-label text-md-right">{{ __('Rua, Av ou Logradouro:')}}</label>
-
-              <div class="col-md-6">
-                <input id="rua" type="text" class="form-control"  name="rua" value="{{ old('rua') }}" required autocomplete="rua" autofocus>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="cidade">{{ __('Cidade:') }}</label>
+                <input id="cidade" type="text" class="form-control"  name="cidade" value="{{ old('cidade') }}" required autocomplete="cidade">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="estado">{{ __('Estado:') }}</label>
+                <input id="estado" type="text" class="form-control"  name="estado" value="{{ old('estado') }}" required autocomplete="estado">
+              </div>
+              <div class="form-group col-md-2">
+                <label for="cep">{{ __('CEP:') }}</label>
+                <input id="cep" type="text" class="form-control"  name="cep" value="{{ old('cep') }}" required autocomplete="cep">
               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="numero" class="col-md-4 col-form-label text-md-right">{{ __('Número:')}}</label>
-
-              <div class="col-md-6">
-                <input id="numero" type="text" class="form-control"  name="numero" value="{{ old('numero') }}" required autocomplete="numero" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="bairro" class="col-md-4 col-form-label text-md-right">{{ __('Bairro:') }}</label>
-
-              <div class="col-md-6">
-                <input id="bairro" type="text" class="form-control"  name="bairro" value="{{ old('bairro') }}" required autocomplete="bairro" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="cidade" class="col-md-4 col-form-label text-md-right">{{ __('Cidade:') }}</label>
-
-              <div class="col-md-6">
-                <input id="cidade" type="text" class="form-control"  name="cidade" value="{{ old('cidade') }}" required autocomplete="cidade" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <label for="estado" class="col-md-4 col-form-label text-md-right">{{ __('Estado:') }}</label>
-
-              <div class="col-md-6">
-                <input id="estado" type="text" class="form-control"  name="estado" value="{{ old('estado') }}" required autocomplete="estado" autofocus>
-              </div>
-            </div>
-
-            <div class="form-group row">
-                <label for="complemento" class="col-md-4 col-form-label text-md-right">{{ __('Complemento:') }}</label>
-
-                <div class="col-md-6">
-                    <input id="complemento" type="text" class="form-control"  name="complemento" value="{{ old('complemento') }}" required autocomplete="complemento" autofocus>
-                </div>
+            <div class="form-group">
+              <label for="complemento">{{ __('Complemento:') }}</label>
+              <input id="complemento" type="text" class="form-control"  name="complemento" value="{{ old('complemento') }}" autocomplete="complemento">
             </div>
             
-            <div class="form-group row mb-0">
-              <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-success">
-                          {{ __('Cadastrar-se!') }}
-                      </button>
-                  </div>
+            <div class="form-group row p-5">
+              <button type="submit" class="btn btn-success btn-lg btn-block">
+                {{ __('Cadastrar-se!') }}
+              </button>
             </div>
           </form>
         </div>
