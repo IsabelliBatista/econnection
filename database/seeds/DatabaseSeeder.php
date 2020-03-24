cbd49e7574f66c4207b209dad872a4bc9711fed7
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-03-23 22:00:22 -0300

    Adicionando seed de marca

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProdutosTableSeeder::class);
+        $this->call(MarcaTableSeeder::class);
+        $this->call(CategoriaTableSeeder::class);
     }
 }
---
+++ database/seeds/MarcaTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MarcaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Marca::create([
+            "name" => "Paz em Gaia"
+        ]);
+        Marca::create([
+            "name" => "Isecta Shoes"
+        ]);
+        Marca::create([
+            "name" => "Eco Consumo"
+        ]);
+        Marca::create([
+            "name" => "Ideia Crua"
+        ]);
+        Marca::create([
+            "name" => "BeeGreen"
+        ]);
+        Marca::create([
+            "name" => "Korui - Ciclos de Vida"
+        ]);
+        Marca::create([
+            "name" => "Positiv.a"
+        ]);
+        Marca::create([
+            "name" => "So Bags"
+        ]);
+        Marca::create([
+            "name" => "Orgânico Natural"
+        ]);
+        Marca::create([
+            "name" => "Menos 1 Lixo"
+        ]);
+    }
+}
